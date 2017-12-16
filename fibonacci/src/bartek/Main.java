0a5d6d7033f130ecea8ec5c89bb7a68d15afc2eb
Bartek S <kursantjava68@gmail.com>
2017-12-16 20:03:47 +0100

    Fibonacci's number searching method with tests.

---
+++ fibonacci/src/bartek/Main.java
@@ -0,0 +1,20 @@
+package bartek;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Fibonacci fibonacci = new Fibonacci();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter which element of Fibonacci sequence you want to know: ");
+        long n = scanner.nextLong();
+        scanner.close();
+        System.out.println("Recursion method: " + n + " Fibonacci's number is equal to :  " + fibonacci.fibRecursion(n));
+        System.out.println();
+        System.out.println("Iteration method: " + n + " Fibonacci's number is equal to :  " + fibonacci.fibIteration(n));
+
+    }
+}
---
+++ fibonacci/src/test/FibonacciTest.java
@@ -0,0 +1,30 @@
+package test;
+
+
+import bartek.Fibonacci;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FibonacciTest {
+
+
+    public Fibonacci fibonacci = new Fibonacci();
+
+    @Test
+    public void fibRecursion() throws Exception {
+
+        long tenthElementOfFibonacci = 55;
+        long expectedElementOfFibonacci = fibonacci.fibRecursion(10);
+
+        Assert.assertEquals(expectedElementOfFibonacci, tenthElementOfFibonacci);
+    }
+
+    @Test
+    public void fibIteration() throws Exception {
+        long tenthElementOfFibonacci = 55;
+        long expectedElementOfFibonacci = fibonacci.fibRecursion(10);
+
+        Assert.assertEquals(expectedElementOfFibonacci, tenthElementOfFibonacci);
+    }
+
+}
