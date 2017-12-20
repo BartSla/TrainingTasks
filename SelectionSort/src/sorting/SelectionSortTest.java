c0df009a06e6699f819e8ec789fe03e6b2325def
Bartek S <kursantjava68@gmail.com>
2017-12-20 21:57:48 +0100

    Some changes

--- SelectionSort/src/sorting/SelectionSortTest.java
+++ SelectionSort/src/sorting/SelectionSortTest.java
@@ -8,8 +8,9 @@ public class SelectionSortTest {
     @Test
     public void selectionSortingTest() {
 
-        int[] givenArray = {1, 2, 3, 4, 5, 6, 7};
+        int[] givenArray = {7, 2, 6, 4, 5, 3, 1};
+        int[] expectedArray= {1, 2, 3, 4, 5, 6, 7};
         int[] selectionExpectedArray = SelectionSort.selectionSort(givenArray);
-        Assert.assertArrayEquals(selectionExpectedArray, givenArray);
+        Assert.assertArrayEquals(selectionExpectedArray, expectedArray);
     }
 }
