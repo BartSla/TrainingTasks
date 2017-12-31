d8b122c1d7cfcdff9f22e7433051120f96d6a42c
Bartek Slabiak <kursantjava68@gmail.com>
2017-12-31 10:51:15 +0100

    changes

--- solutions/src/test/java/pl/coderstrust/inputOutput/FileProcessorTest.java
+++ solutions/src/test/java/pl/coderstrust/inputOutput/FileProcessorTest.java
@@ -1,7 +1,31 @@
 package pl.coderstrust.inputOutput;
 
-import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.List;
 
 public class FileProcessorTest {
+
+    FileProcessor fileProcessor;
+    String path;
+    @BeforeClass
+    public void preper(){
+        fileProcessor = new FileProcessor();
+    path = "";
+    }
+
+    @Test
+    public void readNumbersFromFile (){
+
+//        expected
+        List<Integer> result = fileProcessor.readNumbersFromFile(path);
+
+
+
+    }
+
+
 
 }
