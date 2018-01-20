e739f98b03c5a307e9a971b34c9ea8a12de1542e
Bartek Slabiak <kursantjava68@gmail.com>
2018-01-20 11:43:44 +0100

    I've changed variable to constant.

--- solutions/src/main/java/pl/coderstrust/ipValidation/IpValidation.java
+++ solutions/src/main/java/pl/coderstrust/ipValidation/IpValidation.java
@@ -4,12 +4,12 @@ import java.util.regex.Pattern;
 
 public class IpValidation {
 
-    public final static String ipAddressPattern ="([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])" +
+    public final static String IP_ADDRESS_PATTERN = "([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])" +
             "\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])\\.([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5])";
 
-    Pattern pattern = Pattern.compile(ipAddressPattern);
+    Pattern pattern = Pattern.compile(IP_ADDRESS_PATTERN);
 
     public boolean isIpAddress(String input) {
-        return pattern.matcher(input).matches();
+        return pattern.matcher(IP_ADDRESS_PATTERN).matches();
     }
 }
