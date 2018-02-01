9e84dcb44c90be49e927122330189c470f1e281f
Bartek S <kursantjava68@gmail.com>
2018-02-01 17:44:22 +0100

    Could it be much simpler?

--- solutions/src/main/java/pl/coderstrust/fooBar/FooBar.java
+++ solutions/src/main/java/pl/coderstrust/fooBar/FooBar.java
@@ -6,12 +6,12 @@ public class FooBar {
 
         for (int i = 0; i <= 100; i++) {
 
-            if (i % 3 == 0 && i % 5 != 0 && i % 15 != 0) {
-                System.out.println("Foo " + i);
-            } else if (i % 5 == 0 && i % 15 != 0) {
-                System.out.println("Bar " + i);
-            } else if (i % 15 == 0) {
+            if (i % 15 == 0) {
                 System.out.println("FooBar " + i);
+            } else if (i % 5 == 0) {
+                System.out.println("Bar " + i);
+            } else if (i % 3 == 0) {
+                System.out.println("Foo " + i);
             } else {
                 System.out.println(i);
             }
